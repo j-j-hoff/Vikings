c918f2376e10e9f2c8bd3f1fac4a7afa4704c9d7
Jacob Pettersson <jcbpettersson@gmail.com>
2017-12-28 15:39:47 +0100

    Set up js reletade gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
 var path = require('path');
+var babel = require('gulp-babel');
+var concat = require('gulp-concat');
+var minify = require('gulp-minify');
 
-gulp.task('default', ['less']);
-
+gulp.task('default', ['less', 'pack-js']);
 
 gulp.task('less', function () {
   return gulp.src('./views/less/main.less')
@@ -11,4 +13,12 @@ gulp.task('less', function () {
       paths: [ path.join(__dirname, 'less', 'includes') ]
     }))
     .pipe(gulp.dest('./public/css'));
+});
+
+gulp.task('pack-js', function () {	
+	return gulp.src(['./views/scripts/components/*.js'])
+    .pipe(concat('main.js'))
+    .pipe(babel())
+    .pipe(minify({ ext:{ min:'.js' }, noSource: true }))
+		.pipe(gulp.dest('public/js'));
 });
---
+++ public/js/main.js
@@ -0,0 +1,1 @@
+"use strict";Object.defineProperty(exports,"__esModule",{value:!0});var hello=function(){console.log("Hello")};exports.hello=hello;
