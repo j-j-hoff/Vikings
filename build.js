6527d6a92adae2341a5a5ce54be0e571958be297
Jacob Pettersson <jcbpettersson@gmail.com>
2018-02-08 22:00:35 +0100

    Way to big commit...

---
+++ build.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const config = require('./config');
+const routes = require('./server/routes');
+const fs = require('fs');
+
+console.log('Will build');
+
+fs.writeFile('public/index.html', routes.start(), function (err) {
+  if (err) 
+      return console.log(err);
+  console.log('Wrote start.html');
+});
+
+fs.writeFile('public/reanactment.html', routes.reanactment(), function (err) {
+  if (err) 
+      return console.log(err);
+  console.log('Wrote reanactment.html');
+});
+
+fs.writeFile('public/hire-us.html', routes.hireUs(), function (err) {
+  if (err) 
+      return console.log(err);
+  console.log('Wrote hireUs.html');
+});
+
+fs.writeFile('public/about.html', routes.about(), function (err) {
+  if (err) 
+      return console.log(err);
+  console.log('Wrote about.html');
+});
+
